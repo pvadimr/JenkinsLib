49e20f34debd081f2abd9f03c149b381805b51e7
Vadim Prishlyak <pvadimr@pvadimr.ru>
2025-04-09 18:45:52 +0300

    Fix email function

--- default/vars/reportmail.groovy
+++ default/vars/reportmail.groovy
@@ -2,5 +2,5 @@ import java.text.SimpleDateFormat
 
 def call(String reportToEmail, String Body) {
     sh "echo \"${Body}\" >> email_body.hmtl"
-	emailext attachLog: true, body: '${FILE,path="email_body.hmtl"}', compressLog: true, mimeType: 'text/html', recipientProviders: [requestor()], subject: '$DEFAULT_SUBJECT', to: '${reportToEmail}'
+	emailext attachLog: true, body: '${FILE,path="email_body.hmtl"}', compressLog: true, mimeType: 'text/html', recipientProviders: [requestor()], subject: '$DEFAULT_SUBJECT', to: '$reportToEmail'
 }
