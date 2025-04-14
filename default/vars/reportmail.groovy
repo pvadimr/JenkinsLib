7f8b5337575d077501508c36bdf94d12a9a8b321
Vadim Prishlyak <pvadimr@pvadimr.ru>
2025-04-14 11:31:36 +0300

    FIX-EMAIL
    Fix email function
    Co-authored-by: Vadim Prishlyak <pvadimr@pvadimr.ru>
    Co-committed-by: Vadim Prishlyak <pvadimr@pvadimr.ru>

--- default/vars/reportmail.groovy
+++ default/vars/reportmail.groovy
@@ -2,5 +2,6 @@ import java.text.SimpleDateFormat
 
 def call(String reportToEmail, String Body) {
     sh "echo \"${Body}\" >> email_body.hmtl"
-	emailext attachLog: true, body: '${FILE,path="email_body.hmtl"}', compressLog: true, mimeType: 'text/html', recipientProviders: [requestor()], subject: '$DEFAULT_SUBJECT', to: '$reportToEmail'
+    env.EMAIL_TO = reportToEmail
+	emailext attachLog: true, body: '${FILE,path="email_body.hmtl"}', compressLog: true, mimeType: 'text/html', recipientProviders: [requestor()], subject: '$DEFAULT_SUBJECT', to: EMAIL_TO
 }
