6e7a53dc4e9d4a8005776c7fd3872bf9ee449c34
Vadim Prishlyak <pvadimr@pvadimr.ru>
2025-04-15 15:39:43 +0300

    Add scp functions to ssh

--- default/vars/ssh.groovy
+++ default/vars/ssh.groovy
@@ -14,3 +14,18 @@ void GetKey(String credid) {
 void GeneratePublicKey() {
     sh "ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub"
 }
+
+void SCPTo(String SRC, String DST_HOST, String DST_USER,String DST_PATH)
+{
+    sh "scp -o \"StrictHostKeyChecking no\" ${SRC} ${DST_USER}@${DST_HOST}:${DST_PATH}"
+}
+
+void SCPFrom(String SRC_HOST, String SRC_USER, String SRC_PATH, String DST)
+{
+    sh "scp -o \"StrictHostKeyChecking no\" ${SRC_USER}@${SRC_HOST}:${SRC_PATH} ${DST}"
+}
+
+void SCPBoth(String SRC_HOST, String SRC_USER, String SRC_PATH, String DST_HOST, String DST_USER,String DST_PATH)
+{
+    sh "scp -o \"StrictHostKeyChecking no\" ${SRC_USER}@${SRC_HOST}:${SRC_PATH} ${DST_USER}@${DST_HOST}:${DST_PATH}"
+}
