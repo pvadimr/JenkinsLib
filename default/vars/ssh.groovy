bfc954d3484a52c0c0152b9f3f4715c835da52fe
Vadim Prishlyak <pvadimr@pvadimr.ru>
2025-04-15 13:24:39 +0300

    ssh keys (#3)
    Add functions for handling ssh keys
    
    Reviewed-on: https://forge.pvadimr.com/pvadimr/JenkinsLib/pulls/3
    Co-authored-by: Vadim Prishlyak <pvadimr@pvadimr.ru>
    Co-committed-by: Vadim Prishlyak <pvadimr@pvadimr.ru>

--- default/vars/ssh.groovy
+++ default/vars/ssh.groovy
@@ -1,3 +1,16 @@
 void RunCommand(String HOST, String USER, String COMMAND) {
     sh "ssh -o \"StrictHostKeyChecking no\" ${USER}@${HOST} \"${COMMAND}\""
 }
+
+void GetKey(String credid) {
+    withCredentials([sshUserPrivateKey(credentialsId: credid, keyFileVariable: 'keyfile')]) {
+        sh "mkdir -p ~/.ssh"
+        sh "rm -rf ~/.ssh/id_rsa"
+        sh "cat $keyfile > ~/.ssh/id_rsa"
+        sh "chmod 600 ~/.ssh/id_rsa"
+    }
+}
+
+void GeneratePublicKey() {
+    sh "ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub"
+}
